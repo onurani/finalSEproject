3f58c5e99bc3c6d74de2a874e9d36fc41d1ed5f6
onurani <onurani@iastate.edu>
2022-11-26 17:20:22 -0600

    start of the code

--- FinalProjectTemplate.c
+++ FinalProjectTemplate.c
@@ -2,14 +2,20 @@
 /////////////////////////
 /* 
 Team xx (please insert your team number instead of xx)
-Team member 1 "Name" | "Percentage of Contribution to The Project"
-Team member 2 "Name" | "Percentage of Contribution to The Project"
-Team member 3 "Name" | "Percentage of Contribution to The Project"
-Team member 4 "Name" | "Percentage of Contribution to The Project"
+Team member 1 "Ozair Nurani" | "Percentage of Contribution to The Project"
+Team member 2 "Brendan Kraft" | "Percentage of Contribution to The Project"
+Team member 3 "Chilana A" | "Percentage of Contribution to The Project"
+Team member 4 "Sean F" | "Percentage of Contribution to The Project"
 */
 
 ////////////////////
-#include<stdio.h>//
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+
 //add more here////
 
 //////////////////////
@@ -24,12 +30,22 @@ Team member 4 "Name" | "Percentage of Contribution to The Project"
 /////////////////////////////////
 
 int main(){
+	//usertext is a 2d array 
+	FILE* infile = NULL;
+	const int USER_TEXT_LIMIT = 50;
+	int numrow = 1000;
+	char usertext[numrow][USER_TEXT_LIMIT];
+	infile = fopen("wordList.txt", "r");
 	
+	for(int i = 0; i < numrow; i++){
+		fscanf(infile, "%s", usertext[i]);
+	}
 	
-	
-	
-	
-	
+	for(int j = 0; j < numrow; j++){
+		printf("%s\n", usertext[j]);
+	}
+		
+	fclose(infile);
 	
 	return 0;
 }
