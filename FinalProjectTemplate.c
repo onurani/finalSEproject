42a97e4e21671b162b3ca7c66302a184d367a030
Noisyboy120 <94278518+Noisyboy120@users.noreply.github.com>
2022-11-28 16:12:46 -0600

    added draw maze and draw character function

--- FinalProjectTemplate.c
+++ FinalProjectTemplate.c
@@ -15,6 +15,8 @@ Team member 4 "Sean F" | "Percentage of Contribution to The Project"
 #include <string.h>
 #include <time.h>
 #include <ctype.h>
+#include <curses.h>
+#include <ncurses.h>
 
 //add more here////
 
@@ -22,7 +24,8 @@ Team member 4 "Sean F" | "Percentage of Contribution to The Project"
 //Struct Definition//
 ////////////////////
 
-
+void draw(int x, int y, char use);
+void drawMaze();
 /////////////////////////////////////
 //User Defined Functions Prototype//
 //List prototypes here and define//
@@ -32,7 +35,11 @@ Team member 4 "Sean F" | "Percentage of Contribution to The Project"
 int main()
 {
 	int randomNum;
+	int randomStartPositionX;
 
+
+	initscr();
+    refresh();
 
 	//usertext is a 2d array 
 	FILE* infile = NULL;
@@ -41,20 +48,57 @@ int main()
 	char usertext[numrow][USER_TEXT_LIMIT];
 	infile = fopen("wordList.txt", "r");
 	
+	do{
+	srand(time(NULL));
+	randomNum = (rand() % 1000) + 1;//getting random word
+
 	for(int i = 0; i < numrow; i++){
 		fscanf(infile, "%s", usertext[i]);
+		if(i == randomNum)
+		{
+			printf("Random word is %s", usertext[randomNum]);//print randomword
+			draw(1, 1, usertext[randomNum]);
+		}
 	}
-	
-	/*for(int j = 0; j < numrow; j++){
-		printf("%s\n", usertext[j]);
-	}*/
 
-	randomNum = (rand() % 1000) + 1;
-	printf("Random word is %s", usertext[randomNum]);
+
+
+	drawMaze();
+	}while(1);
+	printf("Random word is %s", usertext[randomNum]);//print randomword
 		
 	fclose(infile);
 	
 	return 0;
+}
+
+void drawMaze()
+{
+        for(int i = 0; i < 23; i++)
+        {
+            draw(0, i, '|');
+        }
+        //drawing right wall
+        for(int i = 0; i < 23; i++)
+        {
+            draw(80 ,i,'|');
+        }
+        //drawing ceiling
+        for(int i = 0; i < 80; i++)
+        {
+            draw(i+1, 24, '-');
+        }
+        for(int i = 0; i < 80; i++)
+        {
+            draw(i, 24, '-');
+        }
+}
+void draw(int x, int y, char use)
+{
+    x = abs(x);
+    y = abs(y);
+    mvaddch(y, x, use);
+    refresh();
 }
 
 ///////////////////////////////////////
